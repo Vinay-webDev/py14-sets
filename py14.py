584f5cc5bd67fc61c04946464c62648b2633592f
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-05-25 23:52:13 +0530

    Update py14.py

--- py14.py
+++ py14.py
@@ -1,33 +1,33 @@
 #sets = colletion which is unordered , unindexed and won't allow duplicate values or elements!
 
-#materials = {"clay","binder","paint","calcium carbide(powder)","water"}
-#tools = {"scrapper","brushes","knife","mould","water"}
+materials = {"clay","binder","paint","calcium carbide(powder)","water"}
+tools = {"scrapper","brushes","knife","mould","water"}
 #some set methods!
-#materials.add("shades")
-#materials.remove("paint")
-#materials.clear()
-#materials.update(tools)
-#tools.update(materials)
-#model = materials.union(tools)
-#model = tools.union(materials)
-#print(materials.difference(tools))
-#print(tools.difference(materials))
-#materials.difference(tools)
-#tools.difference(materials)
-#print(materials.intersection(tools))
-#print(tools.intersection(materials))
+materials.add("shades")
+materials.remove("paint")
+materials.clear()
+materials.update(tools)
+tools.update(materials)
+model = materials.union(tools)
+model = tools.union(materials)
+print(materials.difference(tools))
+print(tools.difference(materials))
+materials.difference(tools)
+tools.difference(materials)
+print(materials.intersection(tools))
+print(tools.intersection(materials))
 
-#for x in materials:
-#    print(x,end=" ")
-#for x in tools:
-#    print(x,end=" ")
+for x in materials:
+    print(x,end=" ")
+for x in tools:
+    print(x,end=" ")
 
-#for x in materials.difference(tools):
-#    print(x,end=" ")
-#for x in tools.difference(materials):
-#    print(x,end=" ")
+for x in materials.difference(tools):
+    print(x,end=" ")
+for x in tools.difference(materials):
+    print(x,end=" ")
 
-#for x in materials.intersection(tools):
-#    print(x,end=" ")
-#for x in tools.intersection(materials):
-#    print(x,end=" ")
+for x in materials.intersection(tools):
+    print(x,end=" ")
+for x in tools.intersection(materials):
+    print(x,end=" ")
